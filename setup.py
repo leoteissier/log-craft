7c23fb5dc6923dcb591789dc32aaec73af4aa744
leoteissier <leo.teissier@numericable.fr>
2024-08-24 23:12:40 +0200

    new version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="log-crafter",
-    version="0.1.0",
+    version="1.0.0",
     author="Teissier Léo",
     author_email="leo.teissier@numericable.fr",
     description="Une bibliothèque pour générer des logs",
