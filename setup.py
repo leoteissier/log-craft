5906dd9e9956107ea4085d71dcaa38f5dbec1bec
leoteissier <leo.teissier@numericable.fr>
2024-08-24 21:07:42 +0200

    new project name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="log-craft",
+    name="log-crafter",
     version="0.1.0",
     author="Teissier Léo",
     author_email="leo.teissier@numericable.fr",
